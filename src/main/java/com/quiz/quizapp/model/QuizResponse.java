9d3226662a7234d19210cbc58b4dd96de790e0ae
Suchismita Nandy <nandysuchismita01@gmail.com>
2025-03-19 12:36:36 +0530

    response class added

---
+++ src/main/java/com/quiz/quizapp/model/QuizResponse.java
@@ -0,0 +1,27 @@
+package com.quiz.quizapp.model;
+
+public class QuizResponse {
+    Integer id;
+    String response;
+
+    public QuizResponse(Integer id, String response) {
+        this.id = id;
+        this.response = response;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
